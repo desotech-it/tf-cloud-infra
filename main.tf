1e52d0f6c7bf58fb43decd00d3fac1aaa03f1342
Gianluca Recchia <g.recchia@desotech.it>
2021-04-02 13:21:03 +0200

    Create VPC and subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_availability_zones" "az" {
+  state = "available"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, 0)
+  availability_zone = data.aws_availability_zones.az.names[0]
+
+  tags = {
+    "Name"      = "pub-subnet"
+    "Workspace" = terraform.workspace
+  }
+}
